ab3b42b8394c1a13a2e700a621b35e926c33ab61
Draylon Vieira <draylid@gmail.com>
2021-04-03 18:44:44 -0300

    teste drone

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+import os
 
-img_r = cv.imread('./img/img_all_characters.jpg',0)
-template = cv.imread('./img/template.png',0)
+#img_r = cv.imread('./img/img_all_characters.jpg',0)
+drone_frames = []#cv.imread('')
+template = cv.imread('./img/drone_template.png',0)
 w, h = template.shape[::-1]
+
+for filename in os.listdir('./img/drone_png/'):
+    img = cv.imread(os.path.join('./img/drone_png/',filename),0)
+    if img is not None:
+        drone_frames.append(img)
 
 # All the 6 methods for comparison in a list
 methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
             'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
 
-for meth in methods:
-    img = img_r.copy()
-    method = eval(meth)
-    # Apply template Matching
-    res = cv.matchTemplate(img,template,method)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(img,top_left, bottom_right, 255, 2)
-    plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
-    plt.show()
+for frame in drone_frames:
+    for meth in methods:
+        img = frame.copy()
+        method = eval(meth)
+        # Apply template Matching
+        res = cv.matchTemplate(img,template,method)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+        if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+            top_left = min_loc
+        else:
+            top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        cv.rectangle(img,top_left, bottom_right, 255, 2)
+        plt.subplot(121),plt.imshow(res,cmap = 'gray')
+        plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+        plt.subplot(122),plt.imshow(img,cmap = 'gray')
+        plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+        plt.suptitle(meth)
+        plt.show()
