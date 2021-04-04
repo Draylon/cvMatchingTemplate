e27b034711e220b59c9659c82456737d0a093b38
Draylon Vieira <draylid@gmail.com>
2021-04-03 23:59:34 -0300

    update fazendo vídeo com rectangle

--- main.py
+++ main.py
@@ -8,32 +8,26 @@ drone_frames = []#cv.imread('')
 template = cv.imread('./img/drone_template.png',0)
 w, h = template.shape[::-1]
 
-for filename in os.listdir('./img/drone_png/'):
-    img = cv.imread(os.path.join('./img/drone_png/',filename),0)
+for i in range(len(os.listdir('./img/drone_png/'))+1):
+    img = cv.imread(os.path.join('./img/drone_png/','drone_render'+str(i)+'.png'),0)
     if img is not None:
         drone_frames.append(img)
 
 # All the 6 methods for comparison in a list
-methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-            'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+#methods = ['cv.TM_CCOEFF']#, 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR','cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+methods = [cv.TM_CCOEFF]
 
-for frame in drone_frames:
-    for meth in methods:
+ii=0
+
+for method in methods:
+    for frame in drone_frames:
         img = frame.copy()
-        method = eval(meth)
-        # Apply template Matching
         res = cv.matchTemplate(img,template,method)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-        if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-            top_left = min_loc
-        else:
-            top_left = max_loc
+        top_left = max_loc
         bottom_right = (top_left[0] + w, top_left[1] + h)
         cv.rectangle(img,top_left, bottom_right, 255, 2)
-        plt.subplot(121),plt.imshow(res,cmap = 'gray')
-        plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-        plt.subplot(122),plt.imshow(img,cmap = 'gray')
-        plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-        plt.suptitle(meth)
-        plt.show()
+        cv.imwrite(os.path.join('./img/drone_rendered/drone_rendered_'+str(ii)+'.png'),img)
+        ii+=1
+
+
